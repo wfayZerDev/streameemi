ff83d74e09a61ec8f87e91f06317ab71cf6ef512
FayZerDv <40219975+wfayZerDev@users.noreply.github.com>
2024-10-07 13:53:37 +0200

    Add Fixture

--- src/DataFixtures/LanguageFixture.php
+++ src/DataFixtures/LanguageFixture.php
@@ -4,9 +4,9 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use App\Entity\Categorie;
+use App\Entity\Language;
 
-class CategoryFixture extends Fixture
+class LanguageFixture extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
@@ -17,10 +17,41 @@ class CategoryFixture extends Fixture
             ["name" => "Spanish", "code" => "es"],
             ["name" => "German", "code" => "de"],
             ["name" => "Italian", "code" => "it"],
+            ["name" => "Portuguese", "code" => "pt"],
+            ["name" => "Dutch", "code" => "nl"],
+            ["name" => "Russian", "code" => "ru"],
+            ["name" => "Chinese", "code" => "zh"],
+            ["name" => "Japanese", "code" => "ja"],
+            ["name" => "Korean", "code" => "ko"],
+            ["name" => "Arabic", "code" => "ar"],
+            ["name" => "Turkish", "code" => "tr"],
+            ["name" => "Hindi", "code" => "hi"],
+            ["name" => "Bengali", "code" => "bn"],
+            ["name" => "Urdu", "code" => "ur"],
+            ["name" => "Vietnamese", "code" => "vi"],
+            ["name" => "Thai", "code" => "th"],
+            ["name" => "Swedish", "code" => "sv"],
+            ["name" => "Danish", "code" => "da"],
+            ["name" => "Finnish", "code" => "fi"],
+            ["name" => "Norwegian", "code" => "no"],
+            ["name" => "Polish", "code" => "pl"],
+            ["name" => "Greek", "code" => "el"],
+            ["name" => "Czech", "code" => "cs"],
+            ["name" => "Slovak", "code" => "sk"],
+            ["name" => "Romanian", "code" => "ro"],
+            ["name" => "Hungarian", "code" => "hu"],
+            ["name" => "Bulgarian", "code" => "bg"],
+            ["name" => "Serbian", "code" => "sr"],
+            ["name" => "Croatian", "code" => "hr"],
+            ["name" => "Bosnian", "code" => "bs"],
+            ["name" => "Albanian", "code" => "sq"],
+            ["name" => "Macedonian", "code" => "mk"],
+            ["name" => "Slovenian", "code" => "sl"],
+            ["name" => "Estonian", "code" => "et"]
         ];
 
         foreach($languages as $language){
-            $entity = new Categorie();
+            $entity = new Language();
             $entity->setName($language["name"]);
             $entity->setCode($language["code"]);
             $manager->persist($entity);
--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Enum\UserStatusEnum;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\User;
@@ -11,9 +12,26 @@ class UserFixture extends Fixture
     public function load(ObjectManager $manager): void
     {
 
+        $users = [
+            ["username" => "Fayzer", "status" => UserStatusEnum::ACTIVE, "email" => "fayzer@gmail.com", "password" => "printemps"],
+            ["username" => "FayzerTest", "status" => UserStatusEnum::INACTIVE, "email" => "fayzerTest@gmail.com", "password" => "printemps"],
+            ["username" => "FayzerProd", "status" => UserStatusEnum::DELETED, "email" => "FayzerProd@gmail.com", "password" => "printemps"],
+            ["username" => "FayzerDev", "status" => UserStatusEnum::PENDING, "email" => "FayzerDev@gmail.com", "password" => "printemps"],
+            ["username" => "FayzerHack", "status" => UserStatusEnum::SUSPENDED, "email" => "FayzerHack@gmail.com", "password" => "printemps"],
+            ["username" => "FayzerNoob", "status" => UserStatusEnum::BANNED, "email" => "FayzerNoob@gmail.com", "password" => "printemps"],
+        ];
+
+        foreach($users as $user){
+            $entity = new User();
+            $entity->setUsername($user["username"]);
+            $entity->setAccountStatus($user["status"]);
+            $entity->setEmail($user["email"]);
+            $entity->setPassword($user["password"]);
+            $manager->persist($entity);
+        }
         // $product = new Product();
         // $manager->persist($product);
-
+        
         $manager->flush();
     }
 }
