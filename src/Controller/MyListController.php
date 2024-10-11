0fa4f754716ff98e213a1b50f39e647accec3e4b
FayZerDv <40219975+wfayZerDev@users.noreply.github.com>
2024-10-11 14:54:49 +0200

    Ajout des modifs fin du cours

--- src/Controller/MyListController.php
+++ src/Controller/MyListController.php
@@ -9,17 +9,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\PlaylistRepository;
 use App\Repository\PlaylistSubscriptionRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class MyListController extends AbstractController
 {
     #[Route('/lists', name: 'show_my_list')]
-    public function discover_category(playlistRepository $playlistRepository, PlaylistSubscriptionRepository $playlistSubscriptionRepository): Response
+    public function discover_category(
+        playlistRepository $playlistRepository, 
+        PlaylistSubscriptionRepository $playlistSubscriptionRepository,
+        Request $request
+        ): Response
     {
+
+        $query = $request->query->get(key: 'playlist');
+
+        $currentPlaylist = $playlistRepository -> find($query);
+
         $playlists = $playlistRepository->findAll();
         $subscriptions = $playlistSubscriptionRepository->findAll();
         return $this->render('lists.html.twig', [
             'playlists' => $playlists,
-            'subscriptions' => $subscriptions
+            'subscriptions' => $subscriptions,
+            'currentPlaylist' => $currentPlaylist
         ]);
     }
 }
